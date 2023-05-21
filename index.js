aa3ebebb25af287c91bed2392e1912dcb743e2a0
shahan <shahan@gmail.com>
2023-05-21 22:08:27 +0500

    feat: obtain request ipaddress, language and userAgent

--- index.js
+++ index.js
@@ -24,6 +24,18 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res) => {
+  const software = req.rawHeaders[3]
+  const language = req.rawHeaders[7]
+  const ipaddress = req.socket.remoteAddress
+  console.log(req.socket.remoteAddress)
+  res.json({
+    ipaddress,
+    language,
+    software
+  })
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
